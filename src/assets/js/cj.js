8fde4570c0204edd58304c1bbd2c688b10d16111
topdev1228 <topdeveloper1228@gmail.com>
2021-06-09 14:45:20 -0700

    Added cjOrderId

--- src/assets/js/cj.js
+++ src/assets/js/cj.js
@@ -4,8 +4,13 @@ $( function() {
         return (value_arr && value_arr.length) ? value_arr.pop() : '';
     };
 
-    var hashEmail = function( email ) {
-      return new Hashes.SHA256().hex(email);
+    var hashEmail = function( email = '') {
+      return new Hashes.SHA256().hex(email.trim().toLowerCase());
+    };
+
+    var hashOrderId = function( email = '' ) {
+        const sha_hash = new Hashes.SHA256().hex( 'order_id: ' + email.trim().toLowerCase() );
+        return new Hashes.MD5().hex( sha_hash );
     };
 
     var cjSendLead = function(data) {
@@ -14,7 +19,7 @@ $( function() {
         var defaults = {
            enterpriseId: '1563474',
            actionTrackerId: '428712'
-       };
+        };
 
         var order = {
             enterpriseId: data.enterpriseId || defaults.enterpriseId,
@@ -23,18 +28,21 @@ $( function() {
             pageType: 'conversionConfirmation',
             amount: 0,
             discount: 0,
-            emailHash: data.email ? hashEmail( data.email.trim().toLowerCase() ) : undefined,
+            emailHash: data.email ? hashEmail( data.email ) : undefined,
+            orderId: data.email ? hashOrderId( data.email ) : undefined,
             // Server side cookie set via Cloudwatch - only on staging/production envs
             cjeventOrder: getCookieValue('cje')
         };
 
         if( window.cjApi ) {
-            console.log( 'Sending lead: ', order );
             return window.cjApi.sendOrder(order);
         }
     };
 
-    $('form.fsForm .fsSubmitButton').on('click', function () {
+    $('form.fsForm .fsSubmitButton').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
         var form = $(this).closest('form.fsForm');
         var email = form.find('input[type="email"]').first().val();
 
